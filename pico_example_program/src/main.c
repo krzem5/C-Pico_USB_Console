67cd44ed170b397a7b810e995d2fc4685508992d
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-04-14 18:44:58 +0200

    Push Update 14/04/2023, 18:44:58

---
+++ pico_example_program/src/main.c
@@ -0,0 +1,28 @@
+#include <hardware/gpio.h>
+#include <hardware/watchdog.h>
+#include <pico/bootrom.h>
+#include <pico_usb_console/protocol.h>
+#include <pico_usb_console/serial.h>
+
+
+
+static void _input_callback(unsigned char length,const char* data){
+	pico_usb_console_protocol_send_log(0,"[%u]: %s",length,data);
+}
+
+
+
+int main(void){
+	gpio_init(PICO_DEFAULT_LED_PIN);
+	gpio_set_dir(PICO_DEFAULT_LED_PIN,GPIO_OUT);
+	gpio_put(PICO_DEFAULT_LED_PIN,1);
+	pico_usb_console_init();
+	pico_usb_console_protocol_set_input_callback(_input_callback);
+	watchdog_enable(500,0);
+	while (1){
+		pico_usb_console_update();
+		pico_usb_console_protocol_update();
+		watchdog_update();
+	}
+	reset_usb_boot(0,0);
+}
